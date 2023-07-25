e797939cb704231acd0522ca890eaeccedcc79e4
Dima Shirkov <dimashirkov@list.ru>
2023-07-25 11:25:32 +0300

    smooth appearance of Yamaguchi in the mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import './App.css'
+import gsap, { Linear } from 'gsap'
+import { useEffect, useRef } from 'react'
 import OperationManual from './components/OperationManual'
 import YamaguchiImage from './components/YamaguchiImage'
 import AppInterface from './components/AppInterface'
 import ContactingAssistant from './components/ContactingAssistant'
+
 function App() {
+
+  const blackHole = useRef();
+  const yamaguchi = useRef();
+
+  useEffect(() => {
+    
+    gsap.fromTo(yamaguchi.current, {
+      translateY: 756,
+    }, {
+      translateY: 0,
+      duration: 3
+    });
+
+    gsap.fromTo(blackHole.current, {
+      scale: 0.2
+    },{
+      scale: 1,
+      duration: 1
+    })
+    
+  })
 
   return (
     <>
       <main className='main-container'>
         <OperationManual/>
-        <YamaguchiImage/>
+        <YamaguchiImage blackHole={blackHole} yamaguchi={yamaguchi}/>
         <AppInterface/> 
         <ContactingAssistant/>       
       </main>
--- src/components/YamaguchiImage.jsx
+++ src/components/YamaguchiImage.jsx
@@ -1,11 +1,14 @@
 import yamaguchi from '../assets/yamaguchi.svg'
 
-const YamaguchiImage = () => {
+const YamaguchiImage = ({ blackHole, yamaguchi: yamaguchiRef }) => {
     return (
-        <div className='wrapper-yamaguchi'>
-            <img src={yamaguchi} className='yamaguchi-image' />
-            <div className='black-hole'></div>
-        </div>
+        <>
+            <div className='wrapper-yamaguchi'>
+                <img src={yamaguchi} className='yamaguchi-image' ref={yamaguchiRef} />
+                <div className='black-hole' ref={blackHole}></div>
+            </div>
+            {/* <div className='black-hole' ref={blackHole}></div> */}
+        </>
     )
 }
 
